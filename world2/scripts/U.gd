2271c7a45ea4f08843cecd4771133f1e99ca05df
ulanovskyi mark <ulanovskyimark@gmail.com>
2024-07-01 00:46:53 +0400

    not

--- world2/scripts/U.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-func delay(secs):
-	await get_tree().create_timer(secs).timeout
-	return
-	
-func getTime():
-	return Time.get_time_dict_from_system().second
--- world2/scripts/console.gd
+++
@@ -1,7 +0,0 @@
-extends Label
-
-func _ready():
-	Global.console = self
-
-func log(newText):
-	text += str("\n") + str(newText)
